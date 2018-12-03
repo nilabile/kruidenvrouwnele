c24746501b6505f4ccd322deb71709c9105d0e03
toon <tk.ano@outlook.be>
2018-12-03 22:02:19 +0100

    cname+email.shroud

---
+++ js/email.shroud.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function(){
+	var username = "nele.keirsmakers";
+	var hostname = "hotmail.com";
+	var linktext = username + "@" + hostname ;
+	$('.emailshroud').html("<a href='" + "mail" + "to:" + username + "@" + hostname + "'>" + linktext + "</a>");
+	
+	var tel1="0475";
+	var tel2="77 65 19"
+	$('.telshroud').html("<a href='" + "tel:" + tel1+tel2.replace(/ /g,'') + "'>" + tel1+' '+tel2 + "</a>");
+});
