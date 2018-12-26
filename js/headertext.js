79d8978dd4c0eb3a26ba60086db7de21c0a70e74
toon <tk.ano@outlook.be>
2018-12-26 15:53:47 +0100

    building content pages

---
+++ js/headertext.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    alert($(location).attr('href'));
+    $.ajax({
+        type: "GET",
+        url: "./data/headertexts.xml",
+        dataType: "xml",
+        success: function (xml) {
+        	var page="index.html";
+        	var headertext=$(xml).find('headertext[page="'+page+'"]').text();
+            $("#headerTextbox").empty();
+            $("#headerTextbox").append(headertext);
+        }
+    });
+});
